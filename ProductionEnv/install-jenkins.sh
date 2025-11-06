f0a65780249e684a47ed510bf0b23dd48a52f2dd
Sarjak Bhandari <bhandarisarjak@gmail.com>
2025-11-06 07:12:00 +0545

    some shell and vagrant file changes

--- ProductionEnv/install-jenkins.sh
+++ ProductionEnv/install-jenkins.sh
@@ -1,17 +1,17 @@
 sudo apt update -y
-sudo apt install fontconfig openjdk-17-jre -y
+sudo apt-get install fontconfig openjdk-17-jre -y
 java -version
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
-sudo apt install unzip
+sudo apt-get install unzip -y
 
 # Download Sonar Scanner
 wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-5.0.1.3006-linux.zip
@@ -20,9 +20,6 @@ wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scann
 unzip sonar-scanner-cli-*.zip
 sudo mv sonar-scanner-*/ /opt/sonar-scanner
 
-# Add to PATH
-echo 'export PATH=$PATH:/opt/sonar-scanner/bin' >> ~/.bashrc
-source ~/.bashrc
-
-# Test
-sonar-scanner --version
+# # Add to PATH
+# echo 'export PATH=$PATH:/opt/sonar-scanner/bin' >> ~/.bashrc
+# source ~/.bashrc
